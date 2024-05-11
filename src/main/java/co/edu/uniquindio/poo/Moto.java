002059cc0a8f14bf890cafd046f59b5ca5aad433
karol <karolm.davilas@uqvirtual.edu.co>
2024-05-10 19:36:57 -0500

    moto

--- src/main/java/co/edu/uniquindio/poo/Moto.java
+++ src/main/java/co/edu/uniquindio/poo/Moto.java
@@ -1,5 +1,16 @@
 package co.edu.uniquindio.poo;
 
-public class Moto {
+public class Moto extends Vehiculo {
+    private int velocidad;
+
+    public Moto (int velocidad){
+        super(modelo, placa);
+        this.velocidad
+    }
+    public int getVelocidad(){
+        return velocidad=velocidad;
+    }
+    public String getTipoMoto(){
+    }
     
 }
