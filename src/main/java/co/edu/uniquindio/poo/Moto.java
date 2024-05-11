2b8581ea69f5c48e4bc040730bf067a7ac798cb9
karol <karolm.davilas@uqvirtual.edu.co>
2024-05-10 19:40:09 -0500

    modificacion moto

--- src/main/java/co/edu/uniquindio/poo/Moto.java
+++ src/main/java/co/edu/uniquindio/poo/Moto.java
@@ -3,7 +3,7 @@ package co.edu.uniquindio.poo;
 public class Moto extends Vehiculo {
     private int velocidad;
 
-    public Moto (int velocidad){
+    public Moto (String modelo, String placa, int velocidad){
         super(modelo, placa);
         this.velocidad
     }
