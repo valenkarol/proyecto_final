35bac4b95d3c884952b9d54998229202cbe4a82d
karol <karolm.davilas@uqvirtual.edu.co>
2024-05-14 17:33:07 -0500

    tipovehiculo

--- src/main/java/co/edu/uniquindio/poo/TipoVehiculo.java
+++ src/main/java/co/edu/uniquindio/poo/TipoVehiculo.java
@@ -1,9 +1,18 @@
 package co.edu.uniquindio.poo;
 
 public enum TipoVehiculo {
-    CARRO(),
-    CLASICA(),
-    HIBRIDA();
+    CARRO(2500),
+    CLASICA(1500),
+    HIBRIDA(2000);
+
+    private final double tarifa;
+
+
+    private TipoVehiculo(double tarifa) {
+        this.tarifa = tarifa;
+    }
+    
+    
 
         
 }
