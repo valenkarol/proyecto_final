8dc3f4a63db44488f8be2eb95904da148df70c3c
karol <karolm.davilas@uqvirtual.edu.co>
2024-05-24 18:19:30 -0500

    registro2

--- src/test/java/co/edu/uniquindio/poo/RegistroTest.java
+++ src/test/java/co/edu/uniquindio/poo/RegistroTest.java
@@ -1,5 +1,20 @@
 package co.edu.uniquindio.poo;
 
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
 public class RegistroTest {
-    
+    private static final Logger LOG = Logger.getLogger(RegistroTest.class.getName());
+
+    @Test
+
+    public void RegistroVehiculo (){
+        LOG.info("Inicio test RegistroVehiculo");
+
+        var propietario= new Propietario("julian", "gutierrez", "1004915388", "jugutier@gmail.com", "3124874139", (byte)51);
+        var carro = new Carro("chevrolet", "234567");
+
+        
+    }
 }
