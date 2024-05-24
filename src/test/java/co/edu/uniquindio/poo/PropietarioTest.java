3078847b876f1fb2ff4c308356633e3abf9edfd8
valentina gutierrez <valentina.gutierrezc@uqvirtual.edu.co>
2024-05-23 20:23:56 -0500

    propietario test final

--- src/test/java/co/edu/uniquindio/poo/PropietarioTest.java
+++ src/test/java/co/edu/uniquindio/poo/PropietarioTest.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.poo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.logging.Logger;
 
@@ -13,7 +14,8 @@ public class PropietarioTest {
     public void DatosCompletos() {
         LOG.info("iniciación test DatosCompletos");
 
-        var propietario = new Propietario("julian", "gutierrez", "1004915388", "jugutier@gmail.com", "3124874139", (byte)51);
+        var propietario = new Propietario("julian", "gutierrez", "1004915388", "jugutier@gmail.com", "3124874139",
+                (byte) 51);
 
         assertEquals("julian", propietario.getNombre());
         assertEquals("gutierrez", propietario.getApellido());
@@ -23,5 +25,43 @@ public class PropietarioTest {
         assertEquals(51, propietario.getEdad());
 
         LOG.info("finalización test DatosCompletos");
+    }
+
+    @Test
+    public void DatosNulos() {
+        LOG.info("iniciación test DatosNulos");
+
+        assertThrows(Throwable.class, () -> new Propietario(null, null, null, null, null, (byte) 51));
+
+        LOG.info("finalización test DatosNulos");
+    }
+
+    @Test
+    public void DatosVacíos() {
+        LOG.info("iniciación test DatosVacíos");
+
+        assertThrows(Throwable.class, () -> new Propietario("", "", "1004915388", "jugutier@gmail.com", "", (byte) 51));
+
+        LOG.info("finalización test DatosVacíos");
+    }
+
+    @Test
+    public void EdadNegativa() {
+        LOG.info("iniciación test EdadNegativa");
+
+        assertThrows(Throwable.class, () -> new Propietario("julian", "gutierrez", "1004915388", "jugutier@gmail.com",
+                "3124874139", (byte) -51));
+
+        LOG.info("finalización test EdadNegativa");
+    }
+
+    @Test
+    public void CorreoInvalido() {
+        LOG.info("iniciación test CorreoNegativo");
+
+        assertThrows(Throwable.class,
+                () -> new Propietario("julian", "gutierrez", "1004915388", "jugutier", "3124874139", (byte) 51));
+
+        LOG.info("finalozación test CorreoInvalido");
     }
 }
