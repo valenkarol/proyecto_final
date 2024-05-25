15f1fd00f1a744838c7ff52251dd408922d537e4
karol <karolm.davilas@uqvirtual.edu.co>
2024-05-24 19:13:04 -0500

    r3

--- src/test/java/co/edu/uniquindio/poo/RegistroTest.java
+++ src/test/java/co/edu/uniquindio/poo/RegistroTest.java
@@ -12,9 +12,12 @@ public class RegistroTest {
     public void RegistroVehiculo (){
         LOG.info("Inicio test RegistroVehiculo");
 
+        var parquedero= new Parqueadero ("parqueadero", (byte)6, (byte)6);
+        var puesto= new Puesto ("2,2");
         var propietario= new Propietario("julian", "gutierrez", "1004915388", "jugutier@gmail.com", "3124874139", (byte)51);
         var carro = new Carro("chevrolet", "234567");
+        var registro= new Registro(null)
 
-        
+
     }
 }
