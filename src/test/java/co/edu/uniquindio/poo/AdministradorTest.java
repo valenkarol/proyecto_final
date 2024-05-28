1792b91d2c78c18f1c73d9939b3ed7fce88361a6
valentina gutierrez <valentina.gutierrezc@uqvirtual.edu.co>
2024-05-27 21:10:27 -0500

    administrador test 2

--- src/test/java/co/edu/uniquindio/poo/AdministradorTest.java
+++ src/test/java/co/edu/uniquindio/poo/AdministradorTest.java
@@ -11,9 +11,10 @@ public class AdministradorTest {
 
     @Test
     public void ObtenerNombre() {
+        // obtener el nombre correcto del dministrador
         LOG.info("iniciación test ObtenerNombre");
 
-        var administrador = new Administrador("jaime", 5000.0);
+        var administrador = new Administrador("jaime", 5000.0, 4000.0, 3500.0);
 
         assertEquals("jaime", administrador.getNombre());
 
@@ -21,5 +22,45 @@ public class AdministradorTest {
 
     }
 
+    @Test
+    public void setTarifaCarro() {
+        // Modificar la tarifa y verificar que la tarifa se haya actualizado correctamente
+        LOG.info("Iniciación test setTarifaCarro");
 
+        var administrador = new Administrador("jaime", 5000.0, 4000.0, 3500.0);
+        double nuevaTarifaCarro = 6000.0;
+        administrador.setTarifaEstacionamientoCarro(nuevaTarifaCarro);
+        
+        assertEquals(nuevaTarifaCarro, administrador.getTarifaEstacionamientoCarro());
+
+        LOG.info("Finalización test setTarifaCarro");
+    }
+
+    @Test
+    public void setTarifaMotoHibrida() {
+        // Modificar la tarifa y verificar que la tarifa se haya actualizado correctamente
+        LOG.info("Iniciación test setTarifaMotoHibrida");
+
+        var administrador = new Administrador("jaime", 5000.0, 4000.0, 3500.0);
+        double nuevaTarifaMotoH = 4500.0;
+        administrador.setTarifaEstacionamientoMotoH(nuevaTarifaMotoH);
+        
+        assertEquals(nuevaTarifaMotoH, administrador.getTarifaEstacionamientoMotoH());
+
+        LOG.info("Finalización test setTarifaMotoHibrida");
+    }
+
+    @Test
+    public void setTarifaMotoClasica() {
+        // Modificar la tarifa y verificar que la tarifa se haya actualizado correctamente
+        LOG.info("Iniciación test setTarifaMotoClasica");
+
+        var administrador = new Administrador("jaime", 5000.0, 4000.0, 3500.0);
+        double nuevaTarifaMotoC = 4000.0;
+        administrador.setTarifaEstacionamientoMotoC(nuevaTarifaMotoC);
+        
+        assertEquals(nuevaTarifaMotoC, administrador.getTarifaEstacionamientoMotoC());
+
+        LOG.info("Finalización test setTarifaMotoClasica");
+    }
 }
